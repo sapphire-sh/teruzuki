4cfe71e6e0ee496921abc64a3b20c482a8176f68
sapphire <info@sapphire.sh>
2016-09-10 14:57:26 +0900

    add akizuki

---
+++ Assets/Scripts/TwitterManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using akizuki;
+
+namespace teruzuki
+{
+    public class TwitterManager : MonoBehaviour
+    {
+        void Start()
+        {
+            Akizuki akizuki = new Akizuki();
+            Debug.Log(Akizuki.Test());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
