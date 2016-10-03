9af4402557a85e632a344521651864018cf62b4d
sapphire <sapphiredev@users.noreply.github.com>
2016-10-03 23:01:56 +0900

    commit

--- Assets/Scripts/AccountManager.cs
+++ Assets/Scripts/AccountManager.cs
@@ -1,60 +1,24 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-using teruzuki.Twitter;
 
 namespace teruzuki
 {
-	public class AccountManager : MonoBehaviour
-	{
-		public RectTransform content;
-		public RectTransform accountItem;
-
-		private IEnumerator initializeItemCoroutine;
-
-		private Client client;
-		private IEnumerator requestTokenCoroutine;
-
-		void Start ()
-		{
-			client = new Client ();
-
-			initializeItemCoroutine = InitializeItem ();
-			StartCoroutine (initializeItemCoroutine);
-		}
-
-		private IEnumerator InitializeItem()
-		{
-			for(var i = 0; i < 20; ++i)
-			{
-				var item = Instantiate (accountItem);
-				item.SetParent (content, false);
-				
-				yield return new WaitForSeconds (0.1f);
+public class AccountManager
+{
+	private static AccountManager instance;
+	public static AccountManager Instance {
+		get {
+			if(instance == null) {
+				instance = new AccountManager();
 			}
-			yield return null;
-		}
-
-		private void RequestTokenCallback (string url)
-		{
-			Application.OpenURL (url);
-		}
-
-		private void AccessTokenCallback ()
-		{
-			SceneManager.LoadScene ("main");
-		}
-
-		public void UIAddNewAccount ()
-		{
-			if (requestTokenCoroutine == null)
-			{
-				requestTokenCoroutine = client.AcquireRequestToken (RequestTokenCallback);
-				StartCoroutine (requestTokenCoroutine);
-			}
+			return instance;
 		}
 	}
+
+	private AccountManager() {
+		AccountList = new List<Account>();
+	}
+
+    public List<Account> AccountList;
+}
 }
---
+++ Assets/Scripts/SplashController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using teruzuki.Twitter;
+
+namespace teruzuki
+{
+	public class SplashController : MonoBehaviour
+	{
+		private IEnumerator initializeCoroutine;
+
+		private Transform logoTransform;
+
+		public void Initialize(Transform transform)
+		{
+			logoTransform = transform;
+
+			initializeCoroutine = Initialize();
+			StartCoroutine(initializeCoroutine);
+		}
+
+		void Update()
+		{
+			if (logoTransform != null)
+			{
+				logoTransform.Rotate(Vector3.up, Time.deltaTime * 100.0f, Space.World);
+			}
+		}
+
+		private IEnumerator Initialize()
+		{
+			TokenManager.Instance.TokenList.ForEach(x => AccountManager.Instance.AccountList.Add(new Account(x)));
+			Debug.Log(AccountManager.Instance.AccountList.Count(x => x.isLoaded));
+			while (AccountManager.Instance.AccountList.Count(x => x.isLoaded) < TokenManager.Instance.TokenList.Count)
+			{
+				yield return new WaitForSeconds(0.1f);
+			}
+			SceneManager.LoadScene("account");
+			yield return null;
+		}
+	}
+}
